6785be2e672791821e42c86a09cc525cd705a77f
ankit <ankit.sanghavi87@gmail.com>
2022-01-30 18:20:04 +0530

    save user with hashed password

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,8 +1,25 @@
-import { ObjectType } from "type-graphql";
-import { BaseEntity, Entity } from "typeorm";
+import { Field, ID, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("users")
 @ObjectType()
-class User extends BaseEntity{
-
+export class User extends BaseEntity{
+    @Field(()=>ID)
+    @PrimaryGeneratedColumn()
+    id: string
+    
+    @Field()
+    @Column("varchar", { length: 255 })
+    firstName:string
+    
+    @Field()
+    @Column("varchar", { length: 255 })
+    lastName:string
+    
+    @Field()
+    @Column("varchar", { length: 255, unique:true })
+    email:string
+    
+    @Column()
+    password:string
 }
