bd44ab7eb1b4a77907f8c6d9d71440e2dd1cc9e5
ankit <ankit.sanghavi87@gmail.com>
2022-02-04 23:54:00 +0530

    password mixin

--- src/modules/user/register/UpdatePasswordInput.ts
+++ src/modules/user/register/UpdatePasswordInput.ts
@@ -1,8 +1,10 @@
 import { PasswordInput } from "../../utils/PasswordInput";
 import { Field, InputType } from "type-graphql";
+import { PasswordMixin } from "../../shared/PasswordMixin";
 
 @InputType()
-export class UpdatePasswordInput extends PasswordInput { 
+// Using a mixin to extend multiple classes in a sort of inheritance - https://youtu.be/j9dOdjBzARo?t=581
+export class UpdatePasswordInput extends PasswordMixin(class {}) { 
     @Field()
     token:string
 }
